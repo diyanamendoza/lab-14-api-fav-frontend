b015496c2d12c75cb0e0e5bdaf0509d8b3788307
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-10-13 17:46:37 -0700

    search page draft

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -4,16 +4,18 @@ export default class search extends Component {
     
     state = {
         name:'',
-
+        result: []
     }
 
     handleSubmit = async(e) => {
         e.preventDefault();
         const result = await getResults(this.state.name);
-        console.log(result)
+        await this.setState({ result })
+        console.log(this.state.result)
     }
 
     render() {
+        const result = this.state.result
         return (
             <div>
                 <form onSubmit = {this.handleSubmit}>
@@ -22,6 +24,15 @@ export default class search extends Component {
                     <button>Search</button>
                     </label>
                 </form>
+                <div className = "results-container">
+                    {result.map(entry => 
+                    <div className="results-item">
+                    {entry.name}
+                    <img src={entry.image} alt={entry.name}/>
+                    </div>
+                    ) }
+                </div>
+
             </div>
         )
     }
